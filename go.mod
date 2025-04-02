104b33a7f793ff7026b56efd7cbe2f4fb758a256
ManogyaDahal <bat16692@gmail.com>
2025-04-02 19:26:14 +0545

    Updated: Code extracts all links of single webpage

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module ManogyaDahal/webCrawler
 
 go 1.24.1
+
+require github.com/jackdanger/collectlinks v0.0.0-20160421202702-24c4ee2870ba
+
+require golang.org/x/net v0.38.0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,44 @@
 package main
 
 import(
+	"fmt"
+	"flag" //flag helps to parse command line args
+	"os"
+	"net/http"
+	"io/ioutil"
+
+	"github.com/jackdanger/collectlinks"
 )
 
 func main(){
+	// Parsing the arguments(webUrl) from the command line
+	flag.Parse()
+	args := flag.Args()
 
+	/* needs a rework here for adding a valid url */
+	if len(args) < 1 {
+		fmt.Println("Please specify the start page to crawl from")
+		os.Exit(1)
+	}
+	// Retreving the page html information 
+	retrieve(args[0])
 }
 
+func retrieve(url string){
+
+	resp, err := http.Get(url)	
+	if err != nil {
+		return
+	}
+	//closes the tcp connection to some web servers (after the function ends)
+	//LIFO
+	defer resp.Body.Close()
+
+	links := collectlinks.All(resp.Body) //using collect links package
+	for _,links := range(links){
+		fmt.Println(links)
+	}
+
+	body, _:= ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+}
