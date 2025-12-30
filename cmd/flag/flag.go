46fbb0593a6187aca6b659477cc76c0ebeedda0f
ManogyaDahal <bat16692@gmail.com>
2025-12-30 20:50:12 +0545

    add: minor changes in implementation

--- cmd/flag/flag.go
+++ cmd/flag/flag.go
@@ -1,7 +1,7 @@
 /* 
 	This package defines all the necessary Flags that our webCrawler needs 
 */
-package main
+package flags 
 
 import (
 	"flag"
@@ -10,31 +10,43 @@ import (
 
 type Config struct {
 	URL 		 		string
-	Depth   		int
-	Limit 			int
+	Depth   		uint
+	Limit 			uint
 	Delay 			time.Duration
-	Concurrency int
+	Concurrency uint
 	Output 			string
 	Verbose 		bool
 }
 
-/* initialize the config of the webcrawler */
+/* initialize the config of the webcrawler with default values*/
 func initConifg() (*Config){ 
 	return &Config{}
 }
 
+/* This function takes args from command line and updates the 
+config struct accordingly */
 func ParseConfig(args []string) (*Config, error){ 
 	fs := flag.NewFlagSet("crawler", flag.ContinueOnError)
 
 	cfg := initConifg()
 
 	fs.StringVar(&cfg.URL, "url", "", "Starting URL to crawl")
-	fs.IntVar(&cfg.Depth, "depth", 2, "Crawl depth")
-	fs.IntVar(&cfg.Limit, "limit", 100, "Max pages to crawl")
+	fs.UintVar(&cfg.Depth, "depth", 2, "Crawl depth")
+	fs.UintVar(&cfg.Limit, "limit", 100, "Max pages to crawl")
 	fs.DurationVar(&cfg.Delay, "delay", 500*time.Millisecond, "Delay between requests")
-	fs.IntVar(&cfg.Concurrency, "concurrency", 4, "Concurrent requests")
+	fs.UintVar(&cfg.Concurrency, "concurrency", 4, "Concurrent requests")
 	fs.StringVar(&cfg.Output, "out", "output.json", "Output file")
 	fs.BoolVar(&cfg.Verbose, "verbose", false, "Verbose logging")
 
+	if err := fs.Parse(args); err != nil { 
+	return nil, err
+	}
+
 	return cfg, nil
+}
+
+func ValidateUserInput(cfg *Config) []error {
+	var errors []error
+	
+	return nil
 }
--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-
+	"log"
+	"os"
+	f "ManogyaDahal/webCrawler/cmd/flag"
 )
 
 func main(){
-	/* todo: initiate flag system */
+	cfg ,err := f.ParseConfig(os.Args[1:])
+	if err != nil { 
+		log.Fatal("err","error occured while parsing the flags", err)
+	}
+	/* initiate flag system */
+	if cfg.URL == "" { 
+		log.Printf("empty url")
+	}else{ log.Printf("non empty url: %s",cfg.URL)}
 }
